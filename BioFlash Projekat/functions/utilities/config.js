af1d06f23094e3bed52f6daee8ef03afa97da8c1
Irhad Fejzic <60628863+namespace-irhad@users.noreply.github.com>
2020-12-07 12:43:00 +0100

    Update config.js

--- BioFlash Projekat/functions/utilities/config.js
+++ BioFlash Projekat/functions/utilities/config.js
@@ -1,11 +1,11 @@
-//Firebase identification
+//Firebase identification (Import from .env file)
 module.exports = {
-  apiKey: "AIzaSyAAFA3h6SB0sJBrdiTyc40H_4WxuEf4uLs",
-  authDomain: "bioflashproject.firebaseapp.com",
-  databaseURL: "https://bioflashproject.firebaseio.com",
-  projectId: "bioflashproject",
-  storageBucket: "bioflashproject.appspot.com",
-  messagingSenderId: "697206632171",
-  appId: "1:697206632171:web:429b209013492ef8cbd8ae",
-  measurementId: "G-EHJJT02HJX",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
